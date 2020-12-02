83ad43945282194a11bfc2d4033cbeaee0b8376e
astrapi69 <asterios.raptis@gmx.net>
2020-12-02 13:47:51 +0100

    added javadoc to methods

--- src/main/java/de/alpharogroup/string/SplitStringExtensions.java
+++ src/main/java/de/alpharogroup/string/SplitStringExtensions.java
@@ -7,18 +7,48 @@ import org.apache.commons.lang3.tuple.Triple;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * The class {@link SplitStringExtensions} provides methods for split string objects.<br>
+ * <br>
+ */
 public final class SplitStringExtensions
 {
 	private SplitStringExtensions(){}
 
-	public static final Map<String, Integer> splitToWords(String input) {
-		String[] split = input.split(" ");
-		Map<String, Integer> stringIntegerMap = MapFactory
-			.newCounterMap(ListFactory.newArrayList(split));
-		Map<String, Integer> wordCount = new TreeMap<>(stringIntegerMap);
-		return wordCount;
+	/**
+	 * Splits the given input string into words and puts them to a counter map
+	 *
+	 * @param input
+	 *            The input string
+	 * @return The map with the words and the count
+	 */
+	public static Map<String, Integer> splitToWordsAndCount(String input) {
+		return splitToWordsAndCount(input, " ");
 	}
 
+	/**
+	 * Splits the given input string into words and puts them to a counter map
+	 *
+	 * @param input
+	 *            The input string
+	 * @param regex
+	 *            The regex string that is used for splitting
+	 * @return The map with the words and the count
+	 */
+	public static Map<String, Integer> splitToWordsAndCount(String input, String regex) {
+		String[] split = input.split(regex);
+		Map<String, Integer> stringIntegerMap = MapFactory
+			.newCounterMap(ListFactory.newArrayList(split));
+		return new TreeMap<>(stringIntegerMap);
+	}
+
+	/**
+	 * Splits the given message string into three parts and put them into a {@link Triple} object
+	 *
+	 * @param message
+	 *            The message string
+	 * @return The splitted message in a {@link Triple} object
+	 */
 	public static Triple<String, String, String> getTripleFromMessage(String message) {
 		int indexOfFirstCurlyBracket = message.indexOf(']');
 		int indexOfEndUsername = message.indexOf(":", indexOfFirstCurlyBracket);
