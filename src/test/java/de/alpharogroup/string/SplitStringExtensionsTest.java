49e067d94f0401342bbd9ee01469e94bfeab5c53
astrapi69 <asterios.raptis@gmx.net>
2020-12-02 13:51:28 +0100

    added some unit tests

--- src/test/java/de/alpharogroup/string/SplitStringExtensionsTest.java
+++ src/test/java/de/alpharogroup/string/SplitStringExtensionsTest.java
@@ -3,11 +3,9 @@ package de.alpharogroup.string;
 import de.alpharogroup.collections.list.ListFactory;
 import de.alpharogroup.collections.map.MapExtensions;
 import de.alpharogroup.collections.map.MapFactory;
-import de.alpharogroup.collections.properties.SortedProperties;
 import de.alpharogroup.file.read.ReadFileExtensions;
 import de.alpharogroup.file.system.SystemFileExtensions;
 import de.alpharogroup.file.write.WriteFileExtensions;
-import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.tuple.Triple;
 import org.testng.annotations.Test;
 
@@ -28,9 +26,9 @@ public class SplitStringExtensionsTest
 {
 
 	/**
-	 * Test method for {@link SplitStringExtensions#splitToWords(String)}
+	 * Test method for {@link SplitStringExtensions#splitToWordsAndCount(String)}
 	 */
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void testSplitToWords() throws IOException
 	{
 		Map<String, String> converted;
@@ -40,7 +38,7 @@ public class SplitStringExtensionsTest
 		// new scenario
 		input = "";
 		String foo = "[21:41, 13.9.2020] Julia: Aber hallo";
-		actual = SplitStringExtensions.splitToWords(input);
+		actual = SplitStringExtensions.splitToWordsAndCount(input);
 		expected = MapFactory.newHashMap();
 		expected.put("", 0);
 		assertEquals(expected, actual);
@@ -57,20 +55,16 @@ public class SplitStringExtensionsTest
 		List<String> linesInList = ReadFileExtensions.readLinesInList(entryFile, Charset.forName("UTF-8"));
 		String inputOriginal = ReadFileExtensions.readFromFile(entryFile);
 		String replace = inputOriginal.replace("\n", " ");
-		replace = replace.replace("[", " ");
-		replace = replace.replace("]", " ");
-		replace = replace.replace(",", " ");
-		replace = replace.replace(".", " ");
-		replace = replace.replace("?", " ");
-		actual = SplitStringExtensions.splitToWords(replace);
-//		List<String> stringList = convertToList(actual);
-//		File output = new File(userTmpDir, "van-convers-statistic.txt");
-//		WriteFileExtensions.writeLinesToFile(output, stringList, "UTF-8");
-//		MapExtensions.sortByValue(actual, false);
-//		converted = convert(actual);
-
-		expected.put("", 0);
-//		assertEquals(expected, actual);
+//		replace = replace.replace("[", " ");
+//		replace = replace.replace("]", " ");
+//		replace = replace.replace(",", " ");
+//		replace = replace.replace(".", " ");
+//		replace = replace.replace("?", " ");
+		actual = SplitStringExtensions.splitToWordsAndCount(replace);
+		Map<String, Integer> stringIntegerMap = MapExtensions.sortByValue(actual, true);
+				List<String> stringList = convertToList(stringIntegerMap);
+		File output = new File(userTmpDir, "van-convers-statistic.txt");
+		WriteFileExtensions.writeLinesToFile(output, stringList, "UTF-8");
 
 	}
 
@@ -85,50 +79,50 @@ public class SplitStringExtensionsTest
 		input = "[21:41, 13.9.2020] Anton: Aber hallo";
 		Triple<String, String, String> tripleFromMessage = SplitStringExtensions.getTripleFromMessage(input);
 		// TODO continue here
+		System.out.println(tripleFromMessage);
 	}
-//
-//	public static Map<String, String> convert(Map<String, Integer> map) {
-//		Map<String,String> newMap =new TreeMap<>();
-//		for (Map.Entry<String, Integer> entry : map.entrySet()) {
-//			if(entry.getValue() instanceof Integer) {
-//				newMap.put(entry.getKey(), entry.getValue().toString());
-//			}
-//		}
-//		return newMap;
-//	}
-//
-//
-//
-//
-//	public static List<String> convertToList(Map<String, Integer> map) {
-//		List<String> list = ListFactory.newArrayList();
-//		for (Map.Entry<String, Integer> entry : map.entrySet()) {
-//			if(entry.getValue() instanceof Integer) {
-////				newMap.put(entry.getKey(), entry.getValue().toString());
-//				list.add(entry.getKey() + "=" + entry.getValue());
-//			}
-//		}
-//		return list;
-//	}
-//
-//	/**
-//	 * Converts the given Map to a Properties object
-//	 *
-//	 * @param map
-//	 *            The map to convert
-//	 * @return The Properties produced from the Map
-//	 */
-//	public static<K, V> Properties toProperties(final Map<K, V> map)
-//	{
-//		final Properties answer = new Properties();
-//		if (map != null) {
-//			for (final Map.Entry<K, V> entry2 : map.entrySet()) {
-//				final Map.Entry<?, ?> entry = entry2;
-//				final Object key = entry.getKey().toString();
-//				final Object value = entry.getValue().toString();
-//				answer.put(key, value);
-//			}
-//		}
-//		return answer;
-//	}
+
+	public static Map<String, String> convert(Map<String, Integer> map) {
+		Map<String,String> newMap =new TreeMap<>();
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			if(entry.getValue() instanceof Integer) {
+				newMap.put(entry.getKey(), entry.getValue().toString());
+			}
+		}
+		return newMap;
+	}
+
+
+
+
+	public static List<String> convertToList(Map<String, Integer> map) {
+		List<String> list = ListFactory.newArrayList();
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			if(entry.getValue() instanceof Integer) {
+				list.add(entry.getKey() + "=" + entry.getValue());
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * Converts the given Map to a Properties object
+	 *
+	 * @param map
+	 *            The map to convert
+	 * @return The Properties produced from the Map
+	 */
+	public static<K, V> Properties toProperties(final Map<K, V> map)
+	{
+		final Properties answer = new Properties();
+		if (map != null) {
+			for (final Map.Entry<K, V> entry2 : map.entrySet()) {
+				final Map.Entry<?, ?> entry = entry2;
+				final Object key = entry.getKey().toString();
+				final Object value = entry.getValue().toString();
+				answer.put(key, value);
+			}
+		}
+		return answer;
+	}
 }
