981ac61a08a63aa21205dda2896f412ecf717cb9
Asterios Raptis <asteri.raptis@gmail.com>
2025-04-17 19:00:51 +0200

    removed moved to its own repository

--- src/test/java/io/github/astrapi69/string/md/dp/MarkdownProcessingStep.java
+++
@@ -1,6 +0,0 @@
-package io.github.astrapi69.string.md.dp;
-
-public interface MarkdownProcessingStep
-{
-	void process(MarkdownContext context);
-}
--- src/test/java/io/github/astrapi69/string/md/dp/SlugStrategy.java
+++
@@ -1,9 +0,0 @@
-package io.github.astrapi69.string.md.dp;
-
-/**
- * Interface to allow pluggable slug strategies (GitHub, Pandoc, etc.)
- */
-interface SlugStrategy
-{
-	String toSlug(String heading);
-}
